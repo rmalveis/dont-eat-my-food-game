4971972e240bdbe804e97a85a5cdbc58dacb7068
Rafael M Ximenes <rmalveis@gmail.com>
2018-03-27 01:47:47 -0300

    Project Intiation
    First commit with project initialization

---
+++ Assets/Scripts/AbstractPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractPlayer : MonoBehaviour {
+
+	public string playerController;
+
+	void FixedUpdate () {
+
+		// Fazer a programaão do player
+
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.AddForce (Vector3.forward * 1.0f, ForceMode.Force);
+	}
+}
